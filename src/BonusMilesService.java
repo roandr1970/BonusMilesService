35f82cc291b1526bae32817e0a06574823ba0e50
roandr1970 <ambal007a@yandex.ru>
2021-08-19 08:21:30 +0300

    First commit

---
+++ src/BonusMilesService.java
@@ -0,0 +1,7 @@
+public class BonusMilesService {
+    public int calculate (int cost){
+        int costMile = 20;
+        int numberBonusMiles = cost/costMile;
+        return (numberBonusMiles);
+    }
+}
